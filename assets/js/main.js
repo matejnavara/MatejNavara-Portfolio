c0d6a03488e486e3682740553885e8373d1e9f0e
Matej <matej.navara@gmail.com>
2021-02-22 02:37:32 +0000

    Fixed terrible/old reorder logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,90 +4,65 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
+  skel
+    .breakpoints({
+      desktop: "(min-width: 737px)",
+      tablet: "(min-width: 737px) and (max-width: 1200px)",
+      mobile: "(max-width: 736px)",
+    })
+    .viewport({
+      breakpoints: {
+        tablet: {
+          width: 1080,
+        },
+      },
+    });
 
-	skel
-		.breakpoints({
-			desktop: '(min-width: 737px)',
-			tablet: '(min-width: 737px) and (max-width: 1200px)',
-			mobile: '(max-width: 736px)'
-		})
-		.viewport({
-			breakpoints: {
-				tablet: {
-					width: 1080
-				}
-			}
-		});
+  $(function () {
+    var $window = $(window),
+      $body = $("body");
 
-	$(function() {
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass("is-loading");
 
-		var	$window = $(window),
-			$body = $('body');
+    $window.on("load", function () {
+      $body.removeClass("is-loading");
+    });
 
-	// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
+    // Fix: Placeholder polyfill.
+    $("form").placeholder();
 
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
+    // Prioritize "important" elements on mobile.
+    skel.on("+mobile -mobile", function () {
+      $.prioritize(
+        ".important\\28 mobile\\29",
+        skel.breakpoint("mobile").active
+      );
+    });
 
-	// Fix: Placeholder polyfill.
-		$('form').placeholder();
+    // CSS polyfills (IE<9).
+    if (skel.vars.IEVersion < 9) $(":last-child").addClass("last-child");
 
-	// Prioritize "important" elements on mobile.
-		skel.on('+mobile -mobile', function() {
-			$.prioritize(
-				'.important\\28 mobile\\29',
-				skel.breakpoint('mobile').active
-			);
-		});
+    // Scrolly.
+    $window.load(function () {
+      var x = parseInt($(".wrapper").first().css("padding-top")) - 100;
 
-	// CSS polyfills (IE<9).
-		if (skel.vars.IEVersion < 9)
-			$(':last-child').addClass('last-child');
+      $("#nav a, .scrolly").scrolly({
+        speed: 1000,
+        offset: x,
+      });
+    });
 
-	// Scrolly.
-		$window.load(function() {
-
-			var x = parseInt($('.wrapper').first().css('padding-top')) - 100;
-
-			$('#nav a, .scrolly').scrolly({
-				speed: 1000,
-				offset: x
-			});
-
-		});
-
-	//Gridify and W3
-		w3.includeHTML();
-		var app = new App();
-		app.init();
-		if(app){
-
-			var imgs = document.images,
-			len = imgs.length,
-			counter = 0;
-
-			[].forEach.call( imgs, function( img ) {
-				img.addEventListener( 'load', incrementCounter, false );
-			} );
-
-			function incrementCounter() {
-				counter++;
-				if ( counter === len ) {
-					app.refreshGrid();
-					$('#asc').val("z_a");
-					console.log( 'All images loaded!' );
-				}
-			}
-			setTimeout(function(){
-				app.refreshGrid();
-				$('#asc').val("z_a");
-			}, 3000);		
-		}
-
-		
-	});
-
+    //Gridify and W3
+    w3.includeHTML();
+    var app = new App();
+    app.init();
+    if (app) {
+      window.addEventListener("load", () => {
+        app.refreshGrid();
+        $("#asc").val("z_a");
+      });
+    }
+  });
 })(jQuery);
