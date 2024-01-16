21810683f8647ad23817d4d74c8618442161c658
Matej <matej.navara@gmail.com>
2024-01-16 22:19:44 +0800

    Last commit of the old portfolio? Rebuild TBC

--- assets/js/projectgrid.js
+++ assets/js/projectgrid.js
@@ -67,8 +67,7 @@ proto(App, {
     var itemsData = [
       {
         name: "Infection Propagation Simulation",
-        desc:
-          "Healthy cells vs Infected cells. Python simulation using the Pygame framework.",
+        desc: "Healthy cells vs Infected cells. Python simulation using the Pygame framework.",
         link: '"portfolio_contagion.html"',
         img: "link-contagion.jpg",
         date: "2015",
@@ -76,8 +75,7 @@ proto(App, {
       },
       {
         name: "Super Awesome Line Game",
-        desc:
-          "The first published Android app from our Greedy Gulls Studio. Developed in Java using the LibGDX framework.",
+        desc: "The first published Android app from our Greedy Gulls Studio. Developed in Java using the LibGDX framework.",
         link: '"portfolio_salg.html"',
         img: "link-salg.jpg",
         date: "2014",
@@ -85,8 +83,7 @@ proto(App, {
       },
       {
         name: "Greedy Gull Game",
-        desc:
-          "Early prototype of the Greedy Gull game created in Java using the LibGDX framework.",
+        desc: "Early prototype of the Greedy Gull game created in Java using the LibGDX framework.",
         link: '"portfolio_greedy.html"',
         img: "link-greedy.jpg",
         date: "2015",
@@ -94,8 +91,7 @@ proto(App, {
       },
       {
         name: "Procedural Puzzle Level Generation",
-        desc:
-          "Final year project for Computer Science (Games) BSc Hons at the University of Brighton. Developed using Unity and C#/Javascript.",
+        desc: "Final year project for Computer Science (Games) BSc Hons at the University of Brighton. Developed using Unity and C#/Javascript.",
         link: '"portfolio_pplf.html"',
         img: "link-pplf.jpg",
         date: "2015",
@@ -103,8 +99,7 @@ proto(App, {
       },
       {
         name: "Whodunit Padma",
-        desc:
-          "Play as Padma, the ship's AI, in this sci-fi murder mystery short listed for the Tranzfuser Fund 2016. Developed in 3 months using Unity/C#/Ink.",
+        desc: "Play as Padma, the ship's AI, in this sci-fi murder mystery short listed for the Tranzfuser Fund 2016. Developed in 3 months using Unity/C#/Ink.",
         link: '"portfolio_padma.html"',
         img: "link-padma.jpg",
         date: "2016",
@@ -112,16 +107,14 @@ proto(App, {
       },
       {
         name: "VR Pharma Project",
-        desc:
-          "Project for multinational pharmaceutical company simulating the effects of sight disorders in VR. Created in Unity for Oculus VR/Google Cardboard.",
+        desc: "Project for multinational pharmaceutical company simulating the effects of sight disorders in VR. Created in Unity for Oculus VR/Google Cardboard.",
         img: "link-vr.jpg",
         date: "2016",
         filter: ["software", "c#", "javascript"],
       },
       {
         name: "Tidal Browser Game",
-        desc:
-          "Created in 8 hours for the EGX Rezzed Creative Assembly Game Jam. Developed using pure Javascript.",
+        desc: "Created in 8 hours for the EGX Rezzed Creative Assembly Game Jam. Developed using pure Javascript.",
         link: '"portfolio_tidal.html"',
         img: "link-tidal.jpg",
         date: "2016",
@@ -129,8 +122,7 @@ proto(App, {
       },
       {
         name: "Novana Beauty Website",
-        desc:
-          "Bespoke website, branding and promotional material for London based beauty studio.",
+        desc: "Bespoke website, branding and promotional material for London based beauty studio.",
         link: '"http://www.novanabeauty.com/" target="_blank"',
         img: "web_novanabeauty.gif",
         date: "2016",
@@ -138,8 +130,7 @@ proto(App, {
       },
       {
         name: "Frosty the No Man",
-        desc:
-          "You are a giant sentient iceberg washed up on a tropical beach. Run as far as you can before you melt. Created in a week whilst sailing the Andaman Sea with American McGee for Pirate Jam 2017.",
+        desc: "You are a giant sentient iceberg washed up on a tropical beach. Run as far as you can before you melt. Created in a week whilst sailing the Andaman Sea with American McGee for Pirate Jam 2017.",
         link: '"portfolio_frosty.html"',
         img: "link-frosty.gif",
         date: "2017",
@@ -147,8 +138,7 @@ proto(App, {
       },
       {
         name: "Planet Sloth",
-        desc:
-          "Plant seeds and grow trees in this reforestation idle game. Created over a weekend in Unity as part of Slow Game Jam 2017.",
+        desc: "Plant seeds and grow trees in this reforestation idle game. Created over a weekend in Unity as part of Slow Game Jam 2017.",
         link: '"portfolio_sloth.html"',
         img: "link-sloth.gif",
         date: "2017",
@@ -164,8 +154,7 @@ proto(App, {
       },
       {
         name: "iTracR",
-        desc:
-          "Commercial timecard and resource management software. Created using Django, MySQL and web technologies.",
+        desc: "Commercial timecard and resource management software. Created using Django, MySQL and web technologies.",
         link: '"https://itracr.com/" target="_blank"',
         img: "link-itracr.jpg",
         date: "2017",
@@ -173,16 +162,14 @@ proto(App, {
       },
       {
         name: "Document Management Microservice",
-        desc:
-          "Headless RESTful document management microservice for the UK Ministry of Justice. Created using SpringBoot and Postgres.",
+        desc: "Headless RESTful document management microservice for the UK Ministry of Justice. Created using SpringBoot and Postgres.",
         img: "link-moj.png",
         date: "2017",
         filter: ["software", "java"],
       },
       {
         name: "eJudiciary",
-        desc:
-          "Bespoke SharePoint platform for the Royal Courts of Justice. Created in close collaboration with high court judges.",
+        desc: "Bespoke SharePoint platform for the Royal Courts of Justice. Created in close collaboration with high court judges.",
         img: "web_rcj.jpg",
         date: "2017",
         filter: ["software", "java"],
@@ -197,8 +184,7 @@ proto(App, {
       },
       {
         name: "Anastasiia Golovina Website",
-        desc:
-          "WordPress website for international opera singer and travel blogger.",
+        desc: "WordPress website for international opera singer and travel blogger.",
         link: '"http://anastasiia-golovina.com/" target="_blank"',
         img: "web_anastasiiagolovina.gif",
         date: "2018",
@@ -206,8 +192,7 @@ proto(App, {
       },
       {
         name: "Stop That Baby",
-        desc:
-          "Asymmetric 1 v 1 local multiplayer on one keyboard or with two controllers. Created for Pirate Jam 2018 whilst sailing the Andaman Sea, surviving storms and sipping SamSong.",
+        desc: "Asymmetric 1 v 1 local multiplayer on one keyboard or with two controllers. Created for Pirate Jam 2018 whilst sailing the Andaman Sea, surviving storms and sipping SamSong.",
         link: '"https://matejnavara.itch.io/stop-that-baby" target="_blank"',
         img: "link-baby.jpg",
         date: "2018",
@@ -216,8 +201,7 @@ proto(App, {
       },
       {
         name: "AFC Website",
-        desc:
-          "Bespoke showcase site for London based fire and security company.",
+        desc: "Bespoke showcase site for London based fire and security company.",
         link: '"https://afcfire.co.uk/" target="_blank"',
         img: "web_afc.gif",
         date: "2019",
@@ -233,8 +217,7 @@ proto(App, {
       },
       {
         name: "Feedr",
-        desc:
-          "React based commerce site and Android/iOS apps for fast growing food tech startup.",
+        desc: "React based commerce site and Android/iOS apps for fast growing food tech startup.",
         link: '"https://feedr.co" target="_blank"',
         img: "link-feedr.png",
         date: "2020",
@@ -242,8 +225,7 @@ proto(App, {
       },
       {
         name: "Messy Crossings",
-        desc:
-          "Simple arcade style timing/reaction game. Inspired by Lemmings and a pedestrian crossing light in Romania. Created whilst sailing the Andaman Sea for Pirate Jam 2020.",
+        desc: "Simple arcade style timing/reaction game. Inspired by Lemmings and a pedestrian crossing light in Romania. Created whilst sailing the Andaman Sea for Pirate Jam 2020.",
         link: '"https://matejnavara.itch.io/messy-crossings" target="_blank"',
         img: "web_crossing.gif",
         date: "2020",
@@ -252,8 +234,7 @@ proto(App, {
       },
       {
         name: "Lettuce Give Back",
-        desc:
-          "Supported local food charity set up their website and delivery online",
+        desc: "Supported local food charity set up their website and delivery online",
         link: '"https://lettucegiveback.org/" target="_blank"',
         img: "web_lettuce.gif",
         date: "2020",
@@ -272,11 +253,11 @@ proto(App, {
       {
         name: "Expired News",
         desc: "Historic New York Times web app built using React/NextJS/Vercel",
-        link: '"https://expired.news/" target="_blank"',
+        //link: '"https://expired.news/" target="_blank"',
         img: "web_news.gif",
         date: "2021",
         filter: ["website", "software"],
-        weblink: true,
+        //weblink: true,
       },
     ];
 
